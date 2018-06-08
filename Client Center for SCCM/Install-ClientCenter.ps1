992c500e30279bc18247d2f8199863f2f29d9376
Alo Press <37367951+alopress@users.noreply.github.com>
2018-06-08 15:40:45 +0300

    Delete Install-ClientCenter.ps1

--- Client Center for SCCM/Install-ClientCenter.ps1
+++
@@ -1,5 +0,0 @@
-Start-Process msiexec.exe -Wait -ArgumentList '/I "Client.Center.for.Configuration.Manager_v1.0.4.2.msi" /qn /norestart LICENSEE="" LICENSEKEY=""'
-
-
-$SelfUpdate = "C:\Program Files\Client Center for Configuration Manager\Plugin_SelfUpdate.dll"
-if (Test-Path $SelfUpdate) { Remove-Item $SelfUpdate }
