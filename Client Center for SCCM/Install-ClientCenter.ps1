a90471435adf5cfd6334c4b18b02c31456f5ad5f
Alo Press <37367951+alopress@users.noreply.github.com>
2018-06-08 15:39:33 +0300

    Update Install-ClientCenter.ps1

--- Client Center for SCCM/Install-ClientCenter.ps1
+++ Client Center for SCCM/Install-ClientCenter.ps1
@@ -1,4 +1,4 @@
-Start-Process msiexec.exe -Wait -ArgumentList '/I "Client.Center.for.Configuration.Manager_v1.0.4.2.msi" /qn /norestart LICENSEE="Alo Press" LICENSEKEY="vRWo-AOvW-AACk-MX0A"'
+Start-Process msiexec.exe -Wait -ArgumentList '/I "Client.Center.for.Configuration.Manager_v1.0.4.2.msi" /qn /norestart LICENSEE="" LICENSEKEY=""'
 
 
 $SelfUpdate = "C:\Program Files\Client Center for Configuration Manager\Plugin_SelfUpdate.dll"
